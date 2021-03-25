676d5fce833a7e4ebd355fa086cb5feeddb7bd10
vivekK <vivekd@buildingiq.com>
2021-03-26 00:37:28 +1100

    added

--- RestAssuredTest/src/test/java/BookStoreApiSteps.java
+++ RestAssuredTest/src/test/java/BookStoreApiSteps.java
@@ -231,7 +231,8 @@ public class BookStoreApiSteps {
     @Step
     public void  getWrongURL()
     {
-        String requestURL = "http://localhost:8082/getTopResultsApi" ;
+//        String requestURL = "http://localhost:8082/getTopResultsApi" ;
+        String requestURL = "http://app:8082/getTopResultsApi" ;
         System.out.println("Befoe the call");
         //String strPostBody =  "{ \"userName\": \"test-user-new5\",  \"password\": \"Test1234!\"}";
         given()
